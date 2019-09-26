7a62906c587840a103c00eb6dc2fcf69d1968439
amrulamin <55226663+amrulamin@users.noreply.github.com>
2019-09-26 18:45:38 +0700

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string password;
+
+    cout << "========= Login =========" << endl;
+    cout << "Masukan password: ";
+    cin >> password;
+
+    // percabangan if/else
+    if (password == "rajinki"){
+        cout << "Selamat datang bos!" << endl;
+    } else {
+        cout << "Password salah, coba lagi!"  << endl;
+    }
+
+    cout << "Terimakasih sudah menggunakan aplikasi ini!" << endl;
+
+    return 0;
+}
